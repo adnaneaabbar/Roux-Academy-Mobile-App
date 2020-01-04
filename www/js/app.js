56cab9a45946b4ff93c2bd4fcef8f28673fa5580
Adnane Aabbar <44345798+adnaneaabbar@users.noreply.github.com>
2020-01-04 19:27:34 +0100

    added the pull down to refresh page feature

--- www/js/app.js
+++ www/js/app.js
@@ -22,6 +22,13 @@ angular.module('starter', ['ionic']) //array of requires
             $scope.artists.splice($scope.artists.indexOf(item), 1);
         };
 
+        $scope.doRefresh = function () {
+            $http.get('js/data.json').success(function(data) {
+                $scope.artists = data.artists;
+                $scope.$broadcast('scroll.refreshComplete');
+            });
+        };
+
         $scope.toggleStar = function (item) {
             item.star = !item.star;
         };
