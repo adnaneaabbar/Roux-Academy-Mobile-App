05a629903b1436d7f2e7f9c7f3aee7599973f4af
Adnane Aabbar <44345798+adnaneaabbar@users.noreply.github.com>
2020-01-05 00:07:39 +0100

    added the calendar controller + functions

--- www/js/app.js
+++ www/js/app.js
@@ -47,6 +47,15 @@ angular.module('starter', ['ionic']) //array of requires
                     }
                 }
             })
+            .state('tabs.calendar', { //child of tabs
+                url: '/calendar',
+                views: {
+                    'calendar-tab': {
+                        templateUrl: 'templates/calendar.html',
+                        controller: 'CalendarController'
+                    }
+                }
+            })
         $urlRouterProvider.otherwise('/tab/home'); //redirect to home if the specified route doesn't exist 
     })
 
@@ -79,6 +88,27 @@ angular.module('starter', ['ionic']) //array of requires
             $scope.moveItem = function (item, fromIndex, toIndex) {
                 $scope.artists.splice(fromIndex, 1);
                 $scope.artists.splice(toIndex, 0, item);
+            };
+        });
+    }])
+    //adding a controller for the Calendar
+    .controller('CalendarController', ['$scope', '$http', '$state', function ($scope, $http, $state) {
+        $http.get('js/data.json').success(function (data) { //storing data in artists variable if successful
+            $scope.calendar = data.calendar;
+
+            $scope.onItemDelete = function (dayIndex, item) {
+                $scope.calendar[dayIndex].schedule.splice($scope.calendar[dayIndex].schedule.indexOf(item), 1);
+            };
+
+            $scope.doRefresh = function () {
+                $http.get('js/data.json').success(function (data) {
+                    $scope.calendar = data.calendar;
+                    $scope.$broadcast('scroll.refreshComplete');
+                });
+            };
+
+            $scope.toggleStar = function (item) {
+                item.star = !item.star;
             };
         });
     }]);
