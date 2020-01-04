0b3eeab1d5f7f0ab2b17b3ef5ccd89420503cf12
Adnane Aabbar <44345798+adnaneaabbar@users.noreply.github.com>
2020-01-04 15:58:23 +0100

    adding default functionalities to the app.js

--- www/js/app.js
+++ www/js/app.js
@@ -1,1 +1,14 @@
 // Ionic Starter App
+
+angular.module('starter', ['ionic']) //array of requires
+.run(function($ionicPlatform) {
+    $ionicPlatform.ready(function () {
+        //Hide accesory bar by default
+        if (window.cordova && window.cordova.plugins.Keyboard) {
+            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+        }
+        if (window.StatusBar) {
+            StatusBar.styleDefault();
+        }
+    });
+})
