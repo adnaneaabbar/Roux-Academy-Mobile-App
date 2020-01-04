efc8b3f7ba4316f96d75f187b5aa43e9aae1124f
Adnane Aabbar <44345798+adnaneaabbar@users.noreply.github.com>
2020-01-04 21:34:18 +0100

    implementing the first route, adding the ion tabs, added templates

--- www/js/app.js
+++ www/js/app.js
@@ -1,41 +1,61 @@
 // Ionic Starter App
 
 angular.module('starter', ['ionic']) //array of requires
-.run(function($ionicPlatform) {
-    $ionicPlatform.ready(function () {
-        //Hide accesory bar by default
-        if (window.cordova && window.cordova.plugins.Keyboard) {
-            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-        }
-        if (window.StatusBar) {
-            StatusBar.styleDefault();
-        }
-    });
-})
+    .run(function ($ionicPlatform) {
+        $ionicPlatform.ready(function () {
+            //Hide accesory bar by default
+            if (window.cordova && window.cordova.plugins.Keyboard) {
+                cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+            }
+            if (window.StatusBar) {
+                StatusBar.styleDefault();
+            }
+        });
+    })
 
-//adding a controller for the module we started in line 3
-.controller('ListController', ['$scope', '$http', function($scope, $http) {
-    $http.get('js/data.json').success(function(data) { //storing data in artists variable if successful
-        $scope.artists = data.artists;
+    //routing
+    .config(function ($stateProvider, $urlRouterProvider) {
+        $stateProvider
+            .state('tabs', {
+                url: '/tab',
+                abstract: true, //shouldn't navigate through it
+                templateUrl: 'templates/tabs.html'
+            })
+            .state('tabs.list', { //child of tabs
+                url: '/list',
+                views: {
+                    'list-tab': {
+                        templateUrl: 'templates/list.html',
+                        controller: 'ListController'
+                    }
+                }
+            })
+        $urlRouterProvider.otherwise('/tab/list'); //redirect to tabs.list if the specified route doesn't exist 
+    })
 
-        $scope.onItemDelete = function (item) {
-            $scope.artists.splice($scope.artists.indexOf(item), 1);
-        };
+    //adding a controller for the module we started in line 3
+    .controller('ListController', ['$scope', '$http', function ($scope, $http) {
+        $http.get('js/data.json').success(function (data) { //storing data in artists variable if successful
+            $scope.artists = data.artists;
 
-        $scope.doRefresh = function () {
-            $http.get('js/data.json').success(function(data) {
-                $scope.artists = data.artists;
-                $scope.$broadcast('scroll.refreshComplete');
-            });
-        };
+            $scope.onItemDelete = function (item) {
+                $scope.artists.splice($scope.artists.indexOf(item), 1);
+            };
 
-        $scope.toggleStar = function (item) {
-            item.star = !item.star;
-        };
+            $scope.doRefresh = function () {
+                $http.get('js/data.json').success(function (data) {
+                    $scope.artists = data.artists;
+                    $scope.$broadcast('scroll.refreshComplete');
+                });
+            };
 
-        $scope.moveItem = function (item, fromIndex, toIndex) {
-            $scope.artists.splice(fromIndex, 1);
-            $scope.artists.splice(toIndex, 0, item);
-        };
-    });
-}]);
+            $scope.toggleStar = function (item) {
+                item.star = !item.star;
+            };
+
+            $scope.moveItem = function (item, fromIndex, toIndex) {
+                $scope.artists.splice(fromIndex, 1);
+                $scope.artists.splice(toIndex, 0, item);
+            };
+        });
+    }]);
